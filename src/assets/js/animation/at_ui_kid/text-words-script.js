9aef983f16c324087d3a1de493f929bde7eea5f0
Тимофей Романович <timofei.web@gmail.com>
2024-08-21 17:37:23 +0100

    add dist

--- src/assets/js/animation/at_ui_kid/text-words-script.js
+++
@@ -1,9 +0,0 @@
-export  const textWordsScript = (textElem) => {
-      let oldHtml = textElem.textContent; 
-      textElem.innerHTML = '<div></div>';
-      textElem.querySelector('div').innerHTML = oldHtml.replace(
-        /\S/g,
-        "<span class='letter'>$&</span>"
-      );
-
-}
