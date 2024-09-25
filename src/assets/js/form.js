a6000eb403eae18c2e08d49f25de2e701b3f8730
Тимофей Романович <timofei.web@gmail.com>
2024-09-25 14:23:58 +0100

    add country

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,7 +1,20 @@
+$(document).ready(function(event) {
+fetch('https://ipapi.co/json/')
+  .then(response => response.json())
+  .then(data => {
+    window.country = data.country_name;
+  })
+  .catch(error => {
+    window.country = "error";
+  });
+});
+
+
 $(document).delegate('form', 'submit', function(event) {
 	event.preventDefault();
 	let lang = navigator.language;
-	let data = $(this).serialize() + '&lang=' + encodeURIComponent(lang);
+	let data = $(this).serialize() + '&lang=' + encodeURIComponent(lang) + '&country=' + encodeURIComponent(window.country);;
+	alert(data);
 	$.ajax({
 		type: "POST",
 		url: '/send.php',
@@ -11,4 +24,5 @@ $(document).delegate('form', 'submit', function(event) {
 			alert('Hi, I will contact you soon!)');
 		}
 	});
+
 });
